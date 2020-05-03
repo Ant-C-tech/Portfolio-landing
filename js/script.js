0d29d2e9091d5ccf20443764c9e33f4759bcd4a0
Ant-C-tech <chivikovanton@gmail.com>
2020-05-03 15:33:28 +0300

    js optimization

--- js/script.js
+++ js/script.js
@@ -7,4 +7,22 @@ slider.slick({
     slidesToScroll: 1,
     arrows: false,
     autoplay: true,
-})
+});
+
+
+// Проверка поддержки атрибута loading браузерами
+(async () => {
+    if ('loading' in HTMLImageElement.prototype) {
+        const images = document.querySelectorAll("img.lazyload");
+        images.forEach(img => {
+            img.src = img.dataset.src;
+        });
+    } else {
+        // Dynamically import the LazySizes library
+        let script = document.createElement("script");
+        script.async = true;
+        script.src =
+            "https://cdnjs.cloudflare.com/ajax/libs/lazysizes/4.1.8/lazysizes.min.js";
+        document.body.appendChild(script);
+    }
+})();
