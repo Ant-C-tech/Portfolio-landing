71c0712d3ae2233b43efbc5b4bc79d944e1520b2
Ant-C-tech <chivikovanton@gmail.com>
2020-05-08 14:46:27 +0300

    remove slick slider

--- js/script.js
+++ js/script.js
@@ -1,14 +1,3 @@
-// Slick slider
-// let slider = $("#testimonials");
-
-// slider.slick({
-//     infinite: true,
-//     slidesToShow: 1,
-//     slidesToScroll: 1,
-//     arrows: false,
-//     autoplay: true,
-// });
-
 
 // Проверка поддержки атрибута loading браузерами
 (async () => {
